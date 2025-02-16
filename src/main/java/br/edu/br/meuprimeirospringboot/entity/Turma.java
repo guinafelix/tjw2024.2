d04ad25e226addcf4eb72c27a235912fe1607211
guinafelix <oguilhermeleitefelix@gmail.com>
2025-02-16 14:59:51 -0300

    refactor: remocao de imports nao utilizados

--- src/main/java/br/edu/br/meuprimeirospringboot/entity/Turma.java
+++ src/main/java/br/edu/br/meuprimeirospringboot/entity/Turma.java
@@ -2,9 +2,7 @@ package br.edu.br.meuprimeirospringboot.entity;
 
 import jakarta.persistence.*;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
